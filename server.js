637771afe491d21925b403e57f3857c2ffa5b057
Khalil Ahmed <mkhalila@outlook.com>
2020-07-30 19:32:00 +0100

    Create register and login endpoints

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const app = express();
+const passport = require("passport");
+const users = require("./routes/api/users");
 
+const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); // DB Config
 const db = require("./config/keys").mongoURI; // Connect to MongoDB
@@ -10,6 +12,11 @@ mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("MongoDB successfully connected"))
   .catch((err) => console.log(err));
-const port = process.env.PORT || 5000;
 
+// Passport middleware
+app.use(passport.initialize()); // Passport config
+require("./config/passport")(passport); // Routes
+app.use("/api/users", users);
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
