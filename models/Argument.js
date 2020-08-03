d422ad59b1ae654ce34ebabbfc0901625d7524c9
Khalil Ahmed <mkhalila@outlook.com>
2020-08-03 23:45:34 +0100

    Make Argument model use User type

--- models/Argument.js
+++ models/Argument.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
+const User = require("./User");
 
 const Schema = mongoose.Schema; // Create Schema
 
 const Argument = new Schema(
   {
     user: {
-      type: String,
+      type: User,
       required: true,
     },
   },
