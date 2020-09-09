9a260274e41158330a1aff16f0592c6f3661a34d
Khalil Ahmed <mkhalila@outlook.com>
2020-09-09 06:30:57 +0100

    Refactor DiscussionView to use own state

--- client/src/components/DiscussionView.js
+++ client/src/components/DiscussionView.js
@@ -3,43 +3,46 @@ import DiscussionCard from "./DiscussionCard";
 import { CounterArgumentCard } from "./CounterArgumentCard";
 import { connect } from "react-redux";
 import { logoutUser } from "../actions/authActions";
+import axios from "axios";
 
 export class DiscussionView extends Component {
   constructor() {
     super();
     this.state = {
-      user: "",
-      date: "",
-      upvotes: [],
-      downvotes: [],
-      argument: "",
-      counterArguments: [],
-      attackRelation: [],
-      errors: {},
+      discussion: {},
     };
   }
 
   componentDidMount() {
-    // const discussion = this.props.location.state.discussion;
+    this.setState({
+      discussion: this.props.location.state.discussion,
+    });
   }
 
   render() {
-    const discussion = this.props.location.state.discussion;
     console.log(this.props.auth);
-    return (
+    let view = (
       <div className="container">
-        <DiscussionCard discussion={discussion} />
-        {discussion.counterArguments.map((counterArgument) => (
-          <CounterArgumentCard
-            key={counterArgument}
-            discussion={discussion._id}
-            counterArgument={counterArgument}
-            isAuthenticated={this.props.auth.isAuthenticated}
-            user={this.props.auth.user.id}
-          />
-        ))}
+        <h1>Problem</h1>
       </div>
     );
+    if (this.state.discussion.counterArguments !== undefined) {
+      view = (
+        <div className="container">
+          <DiscussionCard discussion={this.state.discussion} />
+          {this.state.discussion.counterArguments.map((counterArgument) => (
+            <CounterArgumentCard
+              key={counterArgument}
+              discussion={this.state.discussion._id}
+              counterArgument={counterArgument}
+              isAuthenticated={this.props.auth.isAuthenticated}
+              user={this.props.auth.user.id}
+            />
+          ))}
+        </div>
+      );
+    }
+    return <div>{view}</div>;
   }
 }
 const mapStateToProps = (state) => ({
